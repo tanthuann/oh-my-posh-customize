07d17ba6333263f1dfb8bde676af6821e53cb10a
tanthuann <truongtanthuan99@gmail.com>
2023-09-14 21:25:22 +0700

    feat: remote config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,1 @@
-oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/username/oh-my-posh-customize/develop/.mytheme.omp.json' | Invoke-Expression
